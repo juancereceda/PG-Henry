d5016ae7fe1af09f0d17938efff5f886d6159178
RuthCodina <ruth0066@hotmail.com>
2021-07-23 16:58:08 -0500

    feal(Client/components/ FAQs): Form Completed

--- client/src/components/FAQ's/index.js
+++ client/src/components/FAQ's/index.js
@@ -1,10 +1,51 @@
-import React from "react";
+import React,{useState} from "react";
 import { FaAngleDown } from "react-icons/fa";
-import { StyledFAQs, StyledQuestion ,  StyledAnswer } from './styles';
+import { StyledFAQs, StyledQuestion ,  StyledAnswer, Styledform, Btn, BtnLarge, InputForm, TextForm } from './styles';
 import { QuestionsAndAnswers as QaAs } from "./Q&A"
+import { adminContact } from "../../actions/FAQs";
+import swal from "sweetalert";
+
 
 export function FAQs () {
     const styledButton = { color: "red", position: "absolute", left: "95%"}
+    const [click, setClick]= useState(false)
+    const [info, setInfo]= useState({
+         subject:'',
+         message:''
+    })
+
+    function handleSubject(e){setInfo({...info, subject: e.target.value})}
+    function handleMessage(e){setInfo({...info, message: e.target.value})}
+
+    function handleClick(e){
+        e.preventDefault()
+        setClick(!click)
+    }
+
+    async function handleSubmit(e){
+        e.preventDefault()
+        let msg = await adminContact(info.subject, info.message)
+        if(msg === 'Email sent'){
+          await swal(msg, "Success", "success", {
+            buttons: false,
+            timer: 2000,
+          });
+        window.location.assign("http://localhost:3000/");
+        }
+        else if(msg === 'User not found'){
+            await swal(msg, "Error", "error", {
+                buttons: false,
+                timer: 2000,
+              });
+        }
+        else{
+           await swal("We couldn't send your message", "Error", "error", {
+            buttons: false,
+            timer: 2000,
+          });
+        }
+    }
+
     return (
         <StyledFAQs>
             <h1>Frequently Asked Questions</h1>
@@ -12,6 +53,15 @@ export function FAQs () {
                 <h2>{QandA.Q} <FaAngleDown style={styledButton}/> </h2>
                 <StyledAnswer key={QaAs.indexOf(QandA.A)}>{QandA.A.split(' ').map(el => el.includes('http') ? <a href={el}>here</a> : el).join(' ')}</StyledAnswer>
             </StyledQuestion>)}
+            <BtnLarge onClick={handleClick}>didn't find what you were looking for? take it easy, just mail us</BtnLarge>
+            {click? 
+              <Styledform onSubmit={handleSubmit}>
+                <InputForm name='subject' placeholder='    Subject' onChange={handleSubject} value={info.subject}/> 
+                <TextForm name='message' placeholder='    Write your question, suggestion, concern' onChange={handleMessage} value={info.message}></TextForm>
+                <Btn type='submit'>contact us</Btn>
+              </Styledform>
+              :
+              null}
         </StyledFAQs>
     )
 }
--- client/src/components/FAQ's/styles.js
+++ client/src/components/FAQ's/styles.js
@@ -30,8 +30,90 @@ export const StyledQuestion = styled.div`
     }
 `;
 
+
+
 export const StyledAnswer = styled.div`
     margin-bottom: 20px;
     padding: 0 15px;
     /* display: none; */
-`;
+    `;
+
+export const BtnLarge = styled.button`
+    font-family: Questrial;
+    font-size: 20px;
+    height: 50px;
+    border: none;
+    border-radius: 50px 100px 50px 100px / 25px 50px 25px 50px;
+    h2{
+        margin-bottom: 25px;
+    }
+    box-shadow: 
+    inset 2px 3px 5px rgba(0, 0, 0, 0.3),
+    inset -2px -3px 5px rgba(0, 0, 0, 0.5);
+    background-color: #ea5455;
+    
+    &:hover {
+        background-color: #e5e5e5;
+        box-shadow: 
+        inset -2px -3px 5px rgba(0, 0, 0, 0.3),
+        inset 2px 3px 5px rgba(0, 0, 0, 0.5);
+    }
+    `;
+
+export const Styledform = styled.form`
+       
+       position: relative;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-evenly;
+        margin: 5px 0;
+        padding: 2px 25px;
+        border: 1px solid #293949;
+        background: #293949;
+        line-height: .7;
+        border-radius: 50px 100px 50px 100px / 25px 50px 25px 50px;
+        h2{
+            margin-bottom: 25px;
+        }
+        `;
+
+export const InputForm= styled.input`
+    text-align: center;
+    margin: 15px 0;
+    height: 25px;
+    border-radius: 50px 100px 50px 100px / 25px 50px 25px 50px;
+    h2{
+        margin-bottom: 25px;
+    }
+    `
+export const TextForm= styled.textarea`
+    text-align: center;
+    padding: 50px;
+    margin: 10px 0;
+    height: 70px;
+    border-radius: 50px 100px 50px 100px / 25px 50px 25px 50px;
+    h2{
+        margin-bottom: 25px;
+        }
+`
+ export const Btn = styled.button`
+    margin-left: 30rem ;
+    align-items: center;
+    font-family: Questrial;
+    font-size: 20px;
+    height: 50px;
+    width: 135px;
+    border: none;
+    border-radius: 10px;
+    box-shadow: 
+       inset 2px 3px 5px rgba(0, 0, 0, 0.3),
+       inset -2px -3px 5px rgba(0, 0, 0, 0.5);
+    background-color: #ea5455;
+            
+    &:hover {
+        background-color: #e5e5e5;
+        box-shadow: 
+           inset -2px -3px 5px rgba(0, 0, 0, 0.3),
+           inset 2px 3px 5px rgba(0, 0, 0, 0.5);
+        }
+  `;
